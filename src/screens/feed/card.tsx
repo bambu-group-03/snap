81f8d4ab27faec7be1f36836f0ed162ac07c2775
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-10-22 22:18:01 -0300

    feat(card): change states for each button

--- src/screens/feed/card.tsx
+++ src/screens/feed/card.tsx
@@ -1,31 +1,84 @@
+import {
+  faComment,
+  faHeart,
+  faRetweet,
+  faShare,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React, { useState } from 'react';
 
 import type { Snap } from '@/api';
-import { Button, Pressable, Text, View } from '@/ui';
+import { Button, Image, Pressable, Text, TouchableOpacity, View } from '@/ui';
 
 type Props = Snap & { onPress?: () => void };
 
 export const Card = ({ content, author, onPress = () => {} }: Props) => {
-  const [count, setCount] = useState(0);
-  const like = () => setCount((prevCount) => prevCount + 1);
+  const [likeCount, setCount] = useState(0);
+  const [reSnapCount, setReSnapCount] = useState(0);
+  const [commentCount, setCommentCount] = useState(0);
 
   return (
-    <Pressable
-      className="m-2 block overflow-hidden rounded-xl  bg-neutral-200 p-2 shadow-xl dark:bg-charcoal-900"
-      onPress={onPress}
-    >
-      <View>
-        <Text variant="md" numberOfLines={1} className="font-bold">
-          {`@${author}`}
-        </Text>
-        <Text variant="xs" numberOfLines={3}>
+    <Pressable className="flex shrink-0 p-4 pb-0" onPress={onPress}>
+      <TouchableOpacity className="group block shrink-0">
+        <View className="flex flex-row items-center">
+          <View>
+            <Image
+              className="inline-block h-10 w-10 rounded-full"
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/56934023?v=4',
+              }}
+            />
+          </View>
+          <View className="mx-3">
+            <Text className="text-base font-medium leading-6 text-black">
+              {author}
+              <Text className="text-sm font-medium leading-5 text-gray-400 transition duration-150 ease-in-out group-hover:text-gray-300">
+                {' '}
+                @{author} . 16 April
+              </Text>
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+
+      <View className="pl-16">
+        <Text className="width-auto shrink text-base font-medium text-black">
           {content}
         </Text>
+        <View className="mt-2 flex border-t border-gray-200 pt-2">
+          <View className="w-full">
+            <View className="flex flex-row items-center">
+              <View className="duration-350 flex flex-row items-center text-xs text-gray-400 transition ease-in-out hover:text-blue-400 dark:text-white dark:hover:text-blue-400">
+                <Text className="m-2">{reSnapCount}</Text>
+                <Button
+                  variant="icon"
+                  onPress={() => setReSnapCount(reSnapCount + 1)}
+                  label={<FontAwesomeIcon icon={faRetweet} />}
+                />
+              </View>
+              <View className="duration-350 flex flex-row items-center text-xs text-gray-400 transition ease-in-out hover:text-red-400 dark:text-white dark:hover:text-green-400">
+                <Text className="m-2">{likeCount}</Text>
+                <Button
+                  variant="icon"
+                  onPress={() => setCount(likeCount + 1)}
+                  label={<FontAwesomeIcon icon={faHeart} />}
+                />
+              </View>
+              <View className="duration-350 flex flex-row items-center text-xs text-gray-400 transition ease-in-out hover:text-green-600 dark:text-white dark:hover:text-red-600">
+                <Text className="m-2">{commentCount}</Text>
+                <Button
+                  variant="icon"
+                  onPress={() => setCommentCount(commentCount + 1)}
+                  label={<FontAwesomeIcon icon={faComment} />}
+                />
+              </View>
+              <View className="duration-350 flex flex-1 items-center text-xs text-gray-400 transition ease-in-out hover:text-blue-400 dark:text-white dark:hover:text-blue-400">
+                <FontAwesomeIcon icon={faShare} />
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-      <Button label={'Likes: ' + count.toString()} onPress={like} />
-      <Button label={'Share'} onPress={() => console.log('share')} />
-      <Button label={'reSnap'} onPress={() => console.log('re snap')} />
-      <></>
     </Pressable>
   );
 };
