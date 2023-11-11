e88282fb1ec60c2ccdbac4ebb1dbf49fb9beb91a
Luis Paredes <lparedesr@fi.uba.ar>
2023-11-11 09:14:01 -0300

    Updated profile design

--- src/screens/profile/profile-screen.tsx
+++ src/screens/profile/profile-screen.tsx
@@ -1,13 +1,30 @@
 import { FocusAwareStatusBar } from '@/ui';
 import { ScrollView } from 'react-native-gesture-handler';
 import ProfileScreenView from './profile-view';
+import { UserType } from '@/core/auth/utils';
+
+const MY_PROFILE_PHOTO =
+  'https://avatars.githubusercontent.com/u/40549839?s=400&u=f9968082a38e11cabaeec2033e3ffb3e18395eb6&v=4';
 
 const ProfileScreen = () => {
+  const user: UserType = {
+    bio_msg: '1/4 CEO of Panda.corp',
+    blocked: false,
+    created_at: '2021-10-04T20:00:00.000Z',
+    email: 'luis@gmail.com',
+    first_name: 'Luis',
+    id: '1',
+    last_name: 'Paredes',
+    phone_number: 'xxx',
+    profile_photo_id: MY_PROFILE_PHOTO,
+    username: 'luis',
+  };
+
   return (
     <>
       <FocusAwareStatusBar />
       <ScrollView>
-        <ProfileScreenView />
+        <ProfileScreenView user={user} />
       </ScrollView>
     </>
   );
--- src/screens/profile/profile-view.tsx
+++ src/screens/profile/profile-view.tsx
@@ -1,57 +1,44 @@
+import { UserType } from '@/core/auth/utils';
 import { FocusAwareStatusBar, View, Image, Text } from '@/ui';
 
-const MY_PROFILE_PHOTO =
-  'https://avatars.githubusercontent.com/u/40549839?s=400&u=f9968082a38e11cabaeec2033e3ffb3e18395eb6&v=4';
-
-const ProfileScreenView = () => {
+const ProfileScreenView = ({ user }: { user: UserType }) => {
   return (
     <View>
       <View className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
         <View className="px-6">
           <View className="flex flex-wrap justify-center">
-            <View className="w-full flex justify-center">
+            <View className="w-full flex justify-center items-center text-center">
               <View className="relative">
                 <Image
-                  source={MY_PROFILE_PHOTO}
-                  className="order-1 h-20 w-20 rounded-full"
-                  //   className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                  source={user.profile_photo_id}
+                  className="order-1 h-32 w-32 rounded-full" // Adjusted size to h-32 and w-32
                 />
               </View>
+              <Text className="text-xl font-bold block tracking-wide text-slate-700 text-center">
+                @{user.username}
+              </Text>
             </View>
-            <View className="w-full text-center mt-20">
-              <View className="flex justify-center lg:pt-4 pt-8 pb-0">
+            <View className="w-full text-center mt-5 border-t border-blueGray-200">
+              <View className="flex justify-center lg:pt-4 pt-2 pb-0 flex-row ">
                 <View className="p-3 text-center">
-                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700">
-                    3,360
-                  </Text>
-                  <Text className="text-sm text-slate-400">Photos</Text>
-                </View>
-                <View className="p-3 text-center">
-                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700">
+                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700 text-center">
                     2,454
                   </Text>
                   <Text className="text-sm text-slate-400">Followers</Text>
                 </View>
 
                 <View className="p-3 text-center">
-                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700">
+                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700 text-center">
                     564
                   </Text>
-                  <Text className="text-sm text-slate-400">Following</Text>
-                </View>
-
-                <View className="p-3 text-center">
-                  <Text className="text-xl font-bold block uppercase tracking-wide text-slate-700">
-                    1000000000000
-                  </Text>
-                  <Text className="text-sm text-slate-400">Followers</Text>
+                  <Text className="text-sm text-slate-400 ">Following</Text>
                 </View>
               </View>
             </View>
           </View>
           <View className="text-center mt-2">
             <Text className="text-2xl text-slate-700 font-bold leading-normal mb-1">
-              Luis Paredes
+              {user.first_name} {user.last_name}
             </Text>
             <View className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
               <Text className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75">
@@ -63,7 +50,7 @@ const ProfileScreenView = () => {
             <View className="flex flex-wrap justify-center">
               <View className="w-full px-4">
                 <Text className="font-light leading-relaxed text-slate-600 mb-4">
-                  1/4 CEO of Panda.corp
+                  {user.bio_msg}
                 </Text>
               </View>
             </View>
