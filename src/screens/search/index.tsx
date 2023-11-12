b595ef7ec2d1506b435b9173560c3a39d194f1e8
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-11-12 02:14:20 -0300

    feat(search): input disable dinamically

--- src/screens/search/index.tsx
+++ src/screens/search/index.tsx
@@ -1,12 +1,101 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-import { FocusAwareStatusBar } from '@/ui';
-export const Search = () => {
-  return (
-    <>
-      <FocusAwareStatusBar />
-    </>
-  );
+import { Button, ControlledInput, ScrollView, View } from '@/ui';
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, 'First Name must be at least 3 characters')
+    .max(50, 'First Name cannot exceed 50 characters')
+    .optional(),
+  content: z
+    .string()
+    .min(3, 'Last Name must be at least 3 characters')
+    .max(50, 'Last Name cannot exceed 50 characters')
+    .optional(),
+  hashtag: z
+    .string()
+    .min(3, 'Last Name must be at least 3 characters')
+    .max(50, 'Last Name cannot exceed 50 characters')
+    .optional(),
+});
+
+export type FormType = z.infer<typeof schema>;
+
+export interface SearchFormProps {
+  onSearchSubmit?: SubmitHandler<FormType>;
+}
+
+const whenSearch: SearchFormProps['onSearchSubmit'] = (data) => {
+  console.log(data);
 };
 
-export default Search;
+export const Search: React.FC = () => {
+  return <FormForSearch onSearchSubmit={whenSearch} />;
+};
+
+export const FormForSearch: React.FC<SearchFormProps> = ({
+  onSearchSubmit = () => {},
+}) => {
+  const { handleSubmit, control, watch } = useForm<FormType>({
+    resolver: zodResolver(schema),
+  });
+
+  const watchedFields = watch();
+  const [isAnyFieldFilled, setIsAnyFieldFilled] =
+    React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const anyFieldFilled = Object.values(watchedFields).some(
+      (fieldValue) => fieldValue && fieldValue.trim() !== ''
+    );
+    setIsAnyFieldFilled(anyFieldFilled);
+  }, [watchedFields]);
+
+  const shouldDisableField = (fieldName: string) => {
+    // Deshabilitar el campo si otro campo está lleno (noa vacío)
+    // y este campo específico está vacío
+    const isFieldEmpty =
+      !watchedFields[fieldName] || watchedFields[fieldName].trim() === '';
+    return isAnyFieldFilled && isFieldEmpty;
+  };
+
+  return (
+    <View className="flex-1 p-4">
+      <ScrollView className="flex-1 p-4">
+        <ControlledInput
+          testID="username-input"
+          control={control}
+          name="username"
+          label="username"
+          disabled={shouldDisableField('username')}
+        />
+        <ControlledInput
+          testID="content-input"
+          control={control}
+          name="content"
+          label="content"
+          disabled={shouldDisableField('content')}
+        />
+        <ControlledInput
+          testID="hashtag-input"
+          control={control}
+          name="hashtag"
+          label="hashtag"
+          disabled={shouldDisableField('hashtag')}
+        />
+      </ScrollView>
+
+      <Button
+        testID="search-button"
+        label="Search"
+        onPress={handleSubmit(onSearchSubmit)}
+        variant="primary"
+      />
+    </View>
+  );
+};
