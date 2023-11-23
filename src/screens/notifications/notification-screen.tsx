2c9c89702a94cbdb4d7ff3f435e04620c56c370b
Luis Paredes <lparedesr@fi.uba.ar>
2023-11-23 18:42:08 -0300

    feat: added page titles

--- src/screens/notifications/notification-screen.tsx
+++ src/screens/notifications/notification-screen.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import { FocusAwareStatusBar, View } from '@/ui';
+import { FocusAwareStatusBar, Text, View } from '@/ui';
 
+import MentionScreen from './mentions-view';
 import NotificationView from './notification-view';
 
 const NotionficationScreen = () => {
   return (
     <>
       <FocusAwareStatusBar />
-      <View>
+      <Text className="text-center text-2xl font-bold">Notifications</Text>
+      <View className="mt-1  py-3 text-center">
         <NotificationView />
       </View>
-      {/* <View>
-        <Text>Mentions</Text>
+      <View className="mt-1 border-t border-slate-200 py-3 text-center">
+        <Text className="text-center text-2xl font-bold">Mentions</Text>
         <MentionScreen />
-      </View> */}
+      </View>
     </>
   );
 };
