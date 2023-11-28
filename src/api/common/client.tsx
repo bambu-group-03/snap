ab4907b0f0d78eec47e4f36dc35ae31719769cd6
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2023-11-28 01:57:41 -0300

    chore(client): http redundant headers

--- src/api/common/client.tsx
+++ src/api/common/client.tsx
@@ -2,24 +2,10 @@ import { Env } from '@env';
 import axios from 'axios';
 // Configurar encabezados por defecto para todas las solicitudes
 // Instancia para el microservicio de contenido
-const contentService = axios.create({
-  baseURL: `${Env.API_URL}/content`,
-  headers: {
-    clientToken: 'CLIENT_TOKEN',
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-});
+const contentService = axios.create({ baseURL: `${Env.API_URL}/content` });
 
 // Instancia para el microservicio de identidad
-const identityService = axios.create({
-  baseURL: `${Env.API_URL}/identity`,
-  headers: {
-    clientToken: 'CLIENT_TOKEN',
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-});
+const identityService = axios.create({ baseURL: `${Env.API_URL}/identity` });
 
 contentService.defaults.headers.common.clientToken = 'CLIENT_TOKEN';
 contentService.defaults.headers.common['Content-Type'] = 'application/json';
