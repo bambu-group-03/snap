33f09bee6325992155c2a05a68169d98d60bb1b3
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-06 20:43:56 -0300

    feat: ui improvement

--- src/screens/search/search-navigator.tsx
+++ src/screens/search/search-navigator.tsx
@@ -15,7 +15,7 @@ import SnapList from './snap-list';
 import Users from './users';
 
 export type SearchStackParamList = {
-  SearchView: any;
+  Search: any;
   UserProfile: { user: UserType };
   Users: { users: UserType[] | undefined };
   Followers: { users: UserType[] | undefined };
@@ -34,7 +34,7 @@ const Stack = createNativeStackNavigator<SearchStackParamList>();
 export const SearchNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="SearchView" component={SearchView} />
+      <Stack.Screen name="Search" component={SearchView} />
       <Stack.Screen name="Users" component={Users} />
       <Stack.Screen name="UserProfile" component={ProfileScreen} />
       <Stack.Screen name="Followers" component={InteractionsScreen} />
