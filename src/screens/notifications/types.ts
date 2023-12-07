4c1b3a647f590eb76cc73ee229d88ba553184d9e
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-07 12:16:14 -0300

    feat: made notifications accesable

--- src/screens/notifications/types.ts
+++ src/screens/notifications/types.ts
@@ -3,9 +3,10 @@ import type { UserType } from '@/core/auth/utils';
 export type Notification = {
   id: number;
   user_id: number;
-  type: string;
   title: string;
   content: string;
+  notification_type: string;
+  redirect_id: string;
   created_at: string;
   updated_at: string;
   user: UserType;
